d343e048eed79e0cce36c3f531d6c1eadce9c2f6
Michael <miwanek11199@gmail.com>
2022-02-28 19:48:09 -0800

    add

---
+++ palindrome number.py
@@ -0,0 +1,11 @@
+from typing import List
+
+#parameters
+x = 141      #s/b true
+y = 1331     #s/b true
+z = 112      #s/b false
+
+
+
+def isPalindrome(self, x: int) -> bool:
+    print("placeholder")
