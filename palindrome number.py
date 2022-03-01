1b8a8ef11c90d03ddf8adb82744f9818613bfd5c
Michael <miwanek11199@gmail.com>
2022-02-28 22:31:04 -0800

    added solution

--- palindrome number.py
+++ palindrome number.py
@@ -1,11 +1,30 @@
+from turtle import ycor
 from typing import List
 
+from numpy import zeros
+
 #parameters
-x = 141      #s/b true
+x = 15433451      #s/b true
 y = 1331     #s/b true
 z = 112      #s/b false
 
 
+def isPalindrome(x: int) -> bool:
+    if x < 0 or (x > 0 and x%10 == 0):   
+     return False
 
-def isPalindrome(self, x: int) -> bool:
-    print("placeholder")
+    reversedNum = 0
+
+    while x > reversedNum:
+        reversedNum = reversedNum * 10 + x % 10
+        x = x // 10
+
+    # If x is equal to reversed number then it is a palindrome
+    # If x has odd number of digits, dicard the middle digit before comparing with x
+    # Example, if x = 23132, at the end of for loop x = 23 and reversedNum = 231
+    # So, reversedNum/10 = 23, which is equal to x
+    return True if (x == reversedNum or x == reversedNum // 10) else False
+
+
+test = isPalindrome(x)
+print(test)
