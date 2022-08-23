f88742f4e6b6f4e63590c4d1c7fbe0ede70ea401
Michael <miwanek11199@gmail.com>
2022-08-22 22:22:32 -0700

    add

--- Contains Duplicate - Hash Set.py
+++ Contains Duplicate - Hash Set.py
@@ -1,4 +1,5 @@
 class Solution:
+    
     def containsDuplicate_1(self, nums: list[int]) -> bool:     
                 
         hashset = set()
@@ -8,17 +9,25 @@ class Solution:
                 return True
             hashset.add(number)
         return False
-    
+  
+  
+  
     def containsDuplicate_2(self, nums: list[int]) -> bool:
         return len(set(nums)) < len(nums)
+   
+   
     
 nums = [1,2,3,1]                            # True
 nums2 = [1,2,3,4]                           # False
 nums3 = [1,7,8,2,79,46,34,28,43,2]          # True
 
-print(Solution().containsDuplicate(nums))
-print(Solution().containsDuplicate(nums2))
-print(Solution().containsDuplicate(nums3))
+print(Solution().containsDuplicate_1(nums))
+print(Solution().containsDuplicate_1(nums2))
+print(Solution().containsDuplicate_1(nums3))
+
+print(Solution().containsDuplicate_2(nums))
+print(Solution().containsDuplicate_2(nums2))
+print(Solution().containsDuplicate_2(nums3))
 
 
 
