081e42b8c5a235d2feb6b2cbf463f92d84ad7e90
Michael Iwanek <miwanek11199@gmail.com>
2022-10-05 15:14:20 -0700

    add skeleton container most water

---
+++ Container with most Water.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxArea_brute_force(self, height: list[int]) -> int:
+        pass
+    
+    def maxArea_optimal(self, height: list[int]) -> int:
+        pass
+    
+
+
+
+        
+height = [1,8,6,2,5,4,8,3,7]  # s/b 49
+
+answer = Solution()
+print(answer.maxArea_brute_force(height))
+print(answer.maxArea_optimal(height))
