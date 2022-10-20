243a9c2627e86e39c75af94f50b37465bc636466
Michael Iwanek <miwanek11199@gmail.com>
2022-10-20 16:22:52 -0700

    add solution

---
+++ Permutation in String.py
@@ -0,0 +1,27 @@
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        pass
+
+
+    def checkInclusion_efficient(self, s1: str, s2: str) -> bool:
+                
+        pass
+
+s1 = "ababab"
+s2 = "ababab"
+
+s3 = "ababab"
+s4 = "ababab"
+
+
+print(Solution().checkInclusion(s1,s2))
+print(Solution().checkInclusion(s3,s4))
+
+
+print(Solution().checkInclusion_efficient(s1,s2))
+print(Solution().checkInclusion_efficient(s3,s4))
+
+
+
