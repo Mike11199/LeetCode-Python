62a3e5bad764c83f4356f2cd260a049e1cb3c1bc
Michael Iwanek <miwanek11199@gmail.com>
2022-11-30 13:47:05 -0800

    add reverse polish notation solution

--- Evaluate Reverse Polish Notation.py
+++ Evaluate Reverse Polish Notation.py
@@ -1,11 +1,25 @@
 class Solution:
     def evalRPN(self, tokens: list[str]) -> int:
-        pass
         
+        stack = []
         
-
-
-
+        for c in tokens:          
+              
+            if c == "+":
+                stack.append( stack.pop() + stack.pop() )
+            elif c == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append( b - a)
+            elif c == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif c == "/":
+                a,b = stack.pop(), stack.pop()
+                stack.append(int(b / a))
+            else:
+                stack.append(int(c))
+                
+        return stack[0]
+         
 tokens = ["2","1","+","3","*"]
 tokens2 = ["4","13","5","/","+"]
 tokens3 = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
